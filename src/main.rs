0243bad6714997a9f528881ed964df1aca330414
Jörg Sommer <joerg@jo-so.de>
2019-10-26 13:33:52 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,159 @@
+#![feature(vec_remove_item)]
+use std::{
+    io::{self, Read},
+    process,
+};
+
+mod sudoku;
+use sudoku::{Board, Field};
+
+fn main() {
+    println!("Input initial board setting (space and newline are ignored, \
+              non-digit charaters define empty fields)\n");
+
+    let mut buffer = String::new();
+    {
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        if let Err(err) = handle.read_to_string(&mut buffer) {
+            eprintln!("Failed to read data: {}", err);
+            process::exit(1);
+        }
+    }
+
+    let mut board = board_from_string(&buffer);
+    drop(buffer);
+
+    board.solve();
+
+    print!("\nSolution:\n\n  ");
+    let mut first = true;
+    for (i, e) in board.fields().iter().enumerate() {
+        if first {
+            first = false;
+        } else if i % 27 == 0 {
+            print!("\n\n  ");
+        } else if i % 9 == 0 {
+            print!("\n  ");
+        } else if i % 3 == 0 {
+            print!("   ");
+        }
+
+        match e {
+            Field::Options(_) => print!("."),
+            Field::Value(v) => print!("{}", v),
+        }
+    }
+    println!();
+}
+
+fn board_from_string(data: &str) -> Board {
+    let mut board = Board::new();
+    board.fill(
+        data.chars()
+            .filter(|c| !c.is_whitespace())
+            .map(|c| c.to_digit(10).map(|x| x as u8))
+    );
+    board
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sudoku::Field;
+
+    fn to_string(board: &Board) -> String {
+        board.fields().iter().map(|x| match x {
+            Field::Options(_) => '_',
+            Field::Value(v) => (v + 0x30).into(),
+        }).collect::<String>()
+    }
+
+    const TEST_BOARD : &str =
+        "92_______\
+         5__87____\
+         _38_91___\
+         _5293_16_\
+         _9_____3_\
+         _73_6498_\
+         ___41_25_\
+         ____53__1\
+         _______73";
+
+    #[test]
+    fn fill_board_simple() {
+        let board = board_from_string(TEST_BOARD);
+
+        assert_eq!(to_string(&board), TEST_BOARD);
+    }
+
+    #[test]
+    fn fill_board_placeholder_x() {
+        let board = board_from_string(
+            "92xxxxxxx\
+             5xx87xxxx\
+             x38x91xxx\
+             x5293x16x\
+             x9xxxxx3x\
+             x73x6498x\
+             xxx41x25x\
+             xxxx53xx1\
+             xxxxxxx73"
+        );
+
+        assert_eq!(to_string(&board), TEST_BOARD);
+    }
+
+    #[test]
+    fn fill_board_newline() {
+        let board = board_from_string(
+            "92_______\n\
+             5__87____\n\
+             _38_91___\n\
+             _5293_16_\n\
+             _9_____3_\n\
+             _73_6498_\n\
+             ___41_25_\n\
+             ____53__1\n\
+             _______73\n"
+        );
+
+        assert_eq!(to_string(&board), TEST_BOARD);
+    }
+
+    #[test]
+    fn fill_board_spaces() {
+        let board = board_from_string(
+            "92_ ___\t___
+             5__ 87_\t___
+             _38 _91\t___
+             _52 93_\t16_
+             _9_ ___\t_3_
+             _73 _64\t98_
+             ___ 41_\t25_
+             ___ _53\t__1
+             ___ ___\t_73"
+        );
+
+        assert_eq!(to_string(&board), TEST_BOARD);
+    }
+
+    #[test]
+    fn fill_board_spaces_newline() {
+        let board = board_from_string(
+            "92_ ___\t___\n
+             5__ 87_\t___\n
+             _38 _91\t___\n
+             \n
+             _52 93_\t16_\n
+             _9_ ___\t_3_\n
+             _73 _64\t98_\n
+             \n
+             ___ 41_\t25_\n
+             ___ _53\t__1\n
+             ___ ___\t_73\n"
+        );
+
+        assert_eq!(to_string(&board), TEST_BOARD);
+    }
+}
---
+++ src/sudoku.rs
@@ -0,0 +1,315 @@
+#[derive(Debug)]
+pub enum Field {
+    Value(u8),
+    Options(Vec<u8>),
+}
+
+impl Field {
+    pub fn with_all_options() -> Self {
+        Field::Options(vec![1,2,3,4,5,6,7,8,9])
+    }
+
+    pub fn set(&mut self, val: u8) {
+        assert!(1 <= val && val <= 9, "Invalid field value: {}", val);
+
+        *self = Field::Value(val);
+    }
+
+    pub fn remove_option(&mut self, val: u8) {
+        if let Field::Options(opts) = self {
+            opts.remove_item(&val);
+        }
+    }
+}
+
+pub struct Board {
+    data: Vec<Field>,
+    changed: bool,
+}
+
+impl Board {
+    pub fn new() -> Self {
+        let mut data = Vec::with_capacity(9 * 9);
+        for _ in 0..9 * 9 {
+            data.push(Field::with_all_options());
+        }
+
+        Board {
+            data,
+            changed: false,
+        }
+    }
+
+    fn neighbours(pos: (usize, usize)) -> Vec<(usize, usize)> {
+        let (row, col) = pos;
+        let mut ret = Vec::with_capacity(9 + 9 + 9 - 4 - 3);
+
+        for c in 0..9 {
+            if c != col {
+                ret.push((row, c));
+            }
+        }
+
+        for r in 0..9 {
+            if r != row {
+                ret.push((r, col));
+            }
+        }
+
+        let square_base_row = 3 * (row / 3);
+        let square_base_col = 3 * (col / 3);
+
+        for r in square_base_row .. square_base_row + 3 {
+            if r == row {
+                continue;
+            }
+
+            for c in square_base_col .. square_base_col + 3 {
+                if c != col {
+                    ret.push((r, c));
+                }
+            }
+        }
+
+        ret
+    }
+
+    #[allow(dead_code)]
+    pub fn field(&self, pos: (usize, usize)) -> &Field {
+        &self.data[pos.0 * 9 + pos.1]
+    }
+
+    pub fn fields(&self) -> &[Field] {
+        &self.data
+    }
+
+    fn set_idx(&mut self, idx: usize, val: u8) {
+        self.data[idx].set(val);
+
+        for pos in Self::neighbours((idx / 9, idx % 9)) {
+            self.data[pos.0 * 9 + pos.1].remove_option(val);
+        }
+
+        self.changed = true;
+    }
+
+    #[allow(dead_code)]
+    pub fn set(&mut self, pos: (usize, usize), val: u8) {
+        self.set_idx(pos.0 * 9 + pos.1, val)
+    }
+
+    pub fn fill(&mut self, data: impl Iterator<Item = Option<u8>>) {
+        data
+            .take(self.data.len())
+            .enumerate()
+            .filter_map(|(i, e)| e.map(|x| (i, x)))
+            .for_each(|(i, val)| self.set_idx(i, val as u8));
+    }
+
+    fn solve_with_one_option(&mut self) {
+        self.data
+            .iter()
+            .enumerate()
+            .filter_map(|(idx, fld)| match fld {
+                Field::Options(opts) if opts.len() == 1 => Some((idx, opts[0])),
+                _ => None,
+            })
+            .collect::<Vec<_>>()
+            .iter()
+            .for_each(|(idx, val)| self.set_idx(*idx, *val));
+    }
+
+    fn solve_single_in_neighbourhood(
+        &mut self, positions: impl Iterator<Item = usize>
+    ) {
+        let mut list : [Vec<_>; 9] = Default::default();
+
+        for idx in positions {
+            if let Field::Options(opts) = &self.data[idx] {
+                for num in opts {
+                    list[*num as usize - 1].push(idx);
+                }
+            }
+        }
+
+        for (num, e) in list.iter().enumerate() {
+            if e.len() == 1 {
+                self.set_idx(e[0], num as u8 + 1);
+            }
+        }
+    }
+
+    pub fn solve(&mut self) {
+        loop {
+            self.changed = false;
+            self.solve_with_one_option();
+
+            for row in 0..9 {
+                self.solve_single_in_neighbourhood((0..9).map(|c| row * 9 + c));
+            }
+
+            for col in 0..9 {
+                self.solve_single_in_neighbourhood((0..9).map(|r| r * 9 + col));
+            }
+
+            for square_row in 0..3 {
+                for square_col in 0..3 {
+                    let square_base_row = 3 * square_row;
+                    let square_base_col = 3 * square_col;
+
+                    self.solve_single_in_neighbourhood(
+                        (square_base_row .. square_base_row + 3).flat_map(|r| {
+                            (square_base_col .. square_base_col + 3)
+                                .map(move |c| r * 9 + c)
+                        })
+                    );
+                }
+            }
+
+            if !self.changed {
+                break;
+            }
+        }
+    }
+}
+
+impl Default for Board {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::board_from_string;
+
+    impl Board {
+        fn to_num_vec(&self) -> Vec<u8> {
+            self.fields().iter().map(|x| match x {
+                Field::Options(_) => 0,
+                Field::Value(v) => *v,
+            }).collect::<Vec<_>>()
+        }
+    }
+
+    #[test]
+    fn neighbours_0_0() {
+        assert_eq!(
+            Board::neighbours((0, 0)),
+            vec![
+                (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8),
+                (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0),
+                (1, 1), (1, 2), (2, 1), (2, 2),
+            ]
+        );
+    }
+
+    #[test]
+    fn neighbours_1_1() {
+        assert_eq!(
+            Board::neighbours((1, 1)),
+            vec![
+                (1, 0), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8),
+                (0, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1),
+                (0, 0), (0, 2), (2, 0), (2, 2),
+            ]
+        );
+    }
+
+    #[test]
+    fn neighbours_5_5() {
+        assert_eq!(
+            Board::neighbours((5, 5)),
+            vec![
+                (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 6), (5, 7), (5, 8),
+                (0, 5), (1, 5), (2, 5), (3, 5), (4, 5), (6, 5), (7, 5), (8, 5),
+                (3, 3), (3, 4), (4, 3), (4, 4),
+            ]
+        );
+    }
+
+    #[test]
+    fn neighbours_6_2() {
+        assert_eq!(
+            Board::neighbours((6, 2)),
+            vec![
+                (6, 0), (6, 1), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8),
+                (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (7, 2), (8, 2),
+                (7, 0), (7, 1), (8, 0), (8, 1),
+            ]
+        );
+    }
+
+    #[test]
+    fn solve_hard() {
+        // https://sudoku.tagesspiegel.de/sudoku-sehr-schwer/
+        let mut board = board_from_string(
+            "92.   ...   ...\
+             5..   87.   ...\
+             .38   .91   ...\
+
+             .52   93.   16.\
+             .9.   ...   .3.\
+             .73   .64   98.\
+
+             ...   41.   25.\
+             ...   .53   ..1\
+             ...   ...   .73"
+        );
+        board.solve();
+
+        assert_eq!(
+            board.to_num_vec(),
+            vec![
+                9, 2, 6,    3, 4, 5,    7, 1, 8,
+                5, 4, 1,    8, 7, 2,    3, 9, 6,
+                7, 3, 8,    6, 9, 1,    4, 2, 5,
+
+                8, 5, 2,    9, 3, 7,    1, 6, 4,
+                6, 9, 4,    1, 2, 8,    5, 3, 7,
+                1, 7, 3,    5, 6, 4,    9, 8, 2,
+
+                3, 8, 7,    4, 1, 6,    2, 5, 9,
+                2, 6, 9,    7, 5, 3,    8, 4, 1,
+                4, 1, 5,    2, 8, 9,    6, 7, 3,
+            ]
+        );
+    }
+
+    #[test]
+    fn solve_normal() {
+        // https://sudoku.tagesspiegel.de/
+        let mut board = board_from_string(
+            "..4   ..5  .2.\
+             .52   .36  84.\
+             .16   .82  ...\
+
+             2..   .5.  4..\
+             ...   .1.  73.\
+             641   ...  ..8\
+
+             ...   8..  ..7\
+             12.   ...  ..4\
+             7..   ...  1.9"
+        );
+        board.solve();
+
+        assert_eq!(
+            board.to_num_vec(),
+            vec![
+                8, 7, 4,    1, 9, 5,    6, 2, 3,
+                9, 5, 2,    7, 3, 6,    8, 4, 1,
+                3, 1, 6,    4, 8, 2,    9, 7, 5,
+
+                2, 3, 7,    9, 5, 8,    4, 1, 6,
+                5, 8, 9,    6, 1, 4,    7, 3, 2,
+                6, 4, 1,    3, 2, 7,    5, 9, 8,
+
+                4, 9, 3,    8, 6, 1,    2, 5, 7,
+                1, 2, 8,    5, 7, 9,    3, 6, 4,
+                7, 6, 5,    2, 4, 3,    1, 8, 9,
+            ]
+        );
+    }
+}
